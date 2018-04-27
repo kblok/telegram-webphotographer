66757c0d2394649985eeff44d41e2403771fe737
Dario Kondratiuk <dariokondratiuk@gmail.com>
2018-04-27 09:10:46 -0300

    Bot draft

---
+++ WebPhotographerBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace WebPhotographerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string telegramApiKey = Environment.GetEnvironmentVariable("WEBPHOTOGRAPHER_APIKEY");
+            var botClient = new TelegramBotClient(telegramApiKey);
+
+            botClient.OnMessage += BotClient_OnMessage;
+            Console.WriteLine("Telegram Bot Started\npress any key to exit");
+            Console.ReadLine();
+
+        }
+
+        private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+
+        }
+    }
+}
